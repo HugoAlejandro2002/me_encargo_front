8f55fce54c3a500c59985f36a779e51bf1412a56
ZeinTonconi <zein.tonconi@gmail.com>
2024-07-28 21:15:01 -0400

    Add Variant without API

--- src/pages/Product/GroupProductTable.tsx
+++ src/pages/Product/GroupProductTable.tsx
@@ -1,13 +1,21 @@
-import { Table } from "antd";
+import { Button, Table } from "antd";
 import { getProductsInGroupAPI } from "../../api/group";
 import useProducts from "../../hooks/useProducts";
 import { useEffect, useState } from "react";
 import { getCategoriesAPI } from "../../api/category";
 import { getProductCategoryAPI } from "../../api/product";
+import ProductInfoModal from "./ProductInfoModal";
+import { InfoCircleOutlined } from '@ant-design/icons';
 
 
-const GroupProductTable = ( group: any) => {
+const GroupProductTable = ( {group, onAddVariant} ) => {
     const columns = [
+        {
+            title: '',
+            dataIndex: 'infoButton',
+            key: 'infoButton',
+            width: '5%'
+        },
         {
             title: 'Producto',
             dataIndex: 'producto',
@@ -18,30 +26,51 @@ const GroupProductTable = ( group: any) => {
             title: 'Stock actual',
             dataIndex: 'stockActual',
             key: 'stockActual',
+            width: "20%"
         },
         {
             title: 'Categoría',
             dataIndex: 'categoria',
             key: 'categoria',
+            width: "20%"
         },
         {
             title: 'Precio de venta',
             dataIndex: "precio",
-            key: "precio"
+            key: "precio",
+            width: "20%"
         }
     ];
+
+    
+    const [isModalVisible, setIsModalVisible] = useState<boolean> (false);
+    const [selectedProduct, setSelectedProduct] = useState<any> (null);
+
+    const showModal = (product: any) => {
+        setSelectedProduct(product)
+        setIsModalVisible(true)
+    }
+    const closeModal = () => { 
+        setIsModalVisible(false)
+        setSelectedProduct(null)
+    }
 
     const [products, setProduts] = useState([])
 
     useEffect(() => {
         const fetchProductInCategory = async () => {
-            const productsRes = await getProductsInGroupAPI(group.group.id);
-            console.log(productsRes)
+            const productsRes = await getProductsInGroupAPI(group.id);
             const parseProducts = await Promise.all( productsRes.map(async (product) => {
                 const category = await getProductCategoryAPI(product.id_producto)
                 return ({
+                    infoButton: ( 
+                        <Button type="primary"  onClick={() => showModal(product)}>
+                            <InfoCircleOutlined />
+                        </Button>
+                    ),
+
                     producto: product.nombre_producto,
-                    stockActual: 10,
+                    stockActual: product.stock || 0,
                     categoria: category.categoria,
                     precio: product.precio
                 })   
@@ -49,8 +78,8 @@ const GroupProductTable = ( group: any) => {
             setProduts(parseProducts)
         }
         fetchProductInCategory()
-    }, [group.group.id])
-
+    },[])
+    
 
     return (
         <div style={{margin: '1rem'}}>
@@ -58,8 +87,20 @@ const GroupProductTable = ( group: any) => {
             columns={columns}
             dataSource={products}
             pagination={false}
-            title={ () => <h2>{group.group.name}</h2>}
+            title={ () => (
+                <div className="flex justify-between items-center">
+                    <h2>{group.name}</h2>
+                    <Button type="primary" onClick={onAddVariant}>Agregar Variante</Button>
+                </div>
+            )}
             />
+            {selectedProduct && (
+                <ProductInfoModal
+                    visible={isModalVisible}
+                    onClose={closeModal}
+                    product={selectedProduct}
+                />
+            )}
         </div>
         
 
--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -6,10 +6,13 @@ import ProductFormModal from "./ProductFormModal";
 import useProducts from "../../hooks/useProducts";
 import useGroup from "../../hooks/useGroup";
 import GroupProductTable from "./GroupProductTable";
+import AddVariantModal from "./AddVariantModal";
 
 const Product = () => {
     const [isModalVisible, setIsModalVisible] = useState(false)
+    const [isVariantModalVisible, setIsVariantModalVisible] = useState(false);
     const [refreshKey, setRefreshKey] = useState(0)
+    const [selectedGroup, setSelectedGroup] = useState(null);
     const { groups } = useGroup()
 
     const showModal = () => {
@@ -25,6 +28,23 @@ const Product = () => {
         setRefreshKey(prevKey => prevKey + 1)
     }
 
+    const showVariantModal = (group) => {
+        setSelectedGroup(group);
+        setIsVariantModalVisible(true);
+    };
+
+    const handleVariantCancel = () => {
+        setIsVariantModalVisible(false);
+        setSelectedGroup(null);
+    };
+
+    const handleVariantAdd = (newVariant) => {
+        // Call API to add new variant
+        console.log("New Variant:", newVariant);
+        handleVariantCancel();
+        setRefreshKey(prevKey => prevKey + 1);
+    };
+
     return (
         <div className="p-4">
             <div className="flex justify-between items-center mb-4">
@@ -32,8 +52,12 @@ const Product = () => {
                 <Button onClick={showModal} type='primary'>Agregar Producto</Button>
             </div>
             {
-                groups.map( group => 
-                    <GroupProductTable group = {group}  />
+               groups.map(group => 
+                    <GroupProductTable 
+                        key={group.id}
+                        group={group}
+                        onAddVariant={() => showVariantModal(group)} 
+                    />
                 )
             }
             <ProductFormModal
@@ -41,6 +65,14 @@ const Product = () => {
                 onCancel={handleCancel}
                 onSuccess={handleSuccess}
             />
+            {selectedGroup && (
+                <AddVariantModal
+                    visible={isVariantModalVisible}
+                    onCancel={handleVariantCancel}
+                    onAdd={handleVariantAdd}
+                    group={selectedGroup}
+                />
+            )}
         </div>
     );
 };

0f316e44809c6788a23eeb0515f8bf1428e1afb5
AndresSanchez <sanchezandres8203@gmail.com>
2024-09-25 15:48:36 -0400

    fix: update user context

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -23,7 +23,7 @@ export const UserContextProvider = ({ children }: UserContextProviderProps) => {
   };
   useEffect(() => {
     fetchUser();
-  }, []);
+  }, [user]);
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}

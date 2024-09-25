60d4f0d583972101ff7e7fd76242f48057a1ea49
Andres Sanchez <102560555+Andrezuu@users.noreply.github.com>
2024-09-25 15:17:48 -0400

    Revert "fix: deploy"

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,18 +1,12 @@
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { Button, Form, Input, message } from "antd";
 import { checkLogin } from "../../api/user";
-import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-  const navigate = useNavigate();
   const handleFinish = async (values: any) => {
-    try {
-      const res = await checkLogin(values);
-      message.success("¡Inicio de sesión exitoso!");
-      navigate("/products");
-    } catch (error) {
-      console.error(error);
-    }
+    const res = await checkLogin(values);
+    // TODO: make this homepage
+    message.success("¡Inicio de sesión exitoso!");
   };
 
   return (
